cdb7c439af2307168898c131bdeb2a276f54886b
alucard1792 <dz9217@hotmail.com>
2017-08-28 03:54:58 -0500

    implementado el formulario del login

--- index.php
+++ index.php
@@ -1,10 +1,22 @@
-<!DOCTYPE html>
-<html>
-<head>
-<?php include("template/template.php"); ?>
-	<title></title>
-</head>
-<body>
 
-</body>
-</html>
+ <!DOCTYPE html>
+ <html>
+ <head>
+ <?php include("template/template.php"); ?>
+     <title></title>
+ </head>
+ <body>
+
+        <div class="page-container">
+            <h1>Ingresar</h1>
+            <form action="controllers/Login.php" method="post">
+                <input type="text" name="usuario" class="username" placeholder="Usuario">
+                <input type="password" name="password" class="password" placeholder="Contraseña">
+                <input type="hidden" name="login">
+                <button type="submit">iniciar sesion</button>
+                <div class="error"><span>+</span></div>
+            </form>
+        </div>
+
+ </body>
+ </html>
